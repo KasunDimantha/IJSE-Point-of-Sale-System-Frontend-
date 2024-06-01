9433f1c8b4769d254fcb6a16cefbb96ecbef3abb
KasunDimantha <kasundimantha97@gmail.com>
2024-06-01 08:45:43 +0530

    update app.jsx authprovider and brouser routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
+import { AuthProvider } from './utils/AUthContext'
 
 
 function App() {
 
   return (
     <div>
-      
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
